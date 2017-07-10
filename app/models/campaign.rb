24a560eacdd04e748793b6673938574241066b2b
Kyle Sponheim <kylesponheim@Kyles-MacBook-Pro.local>
2017-07-09 17:30:54 -0700

    check pending invitations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,6 @@
 class Campaign < ActiveRecord::Base
     
   has_many :campaign_memberships, dependent: :destroy
+  has_many :campaign_invitations, dependent: :destroy
   has_many :users, through: :campaign_memberships
 end
