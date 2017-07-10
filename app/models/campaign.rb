aafad8e96f9e89993656475f0b539426167527a8
Kyle Sponheim <kylesponheim@Kyles-MacBook-Pro.local>
2017-07-09 19:34:52 -0700

    only show edit/update options to owners

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,4 @@
 class Campaign < ActiveRecord::Base
-    
   has_many :campaign_memberships, dependent: :destroy
   has_many :campaign_invitations, dependent: :destroy
   has_many :users, through: :campaign_memberships
