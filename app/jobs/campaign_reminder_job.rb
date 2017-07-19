c8016e4ef466a205f3b3152b9c3942dd170fbcdc
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-07-19 16:16:16 -0700

    find each

--- app/jobs/campaign_reminder_job.rb
+++ app/jobs/campaign_reminder_job.rb
@@ -3,7 +3,7 @@ class CampaignReminderJob < ActiveJob::Base
 
   def perform
     Campaign.playing_today.find_each do |campaign|
-      campaign.users.each { |user| CampaignReminderMailer.campaign_reminder_email(user, campaign).deliver_later }
+      campaign.users.find_each { |user| CampaignReminderMailer.campaign_reminder_email(user, campaign).deliver_later }
     end
   end
 end
