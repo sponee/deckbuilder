40931a20361fa45e92e5d866b9ee8bf7ca5d7d90
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-08-01 12:37:40 -0700

    campaign invitation mailer spec

--- spec/mailers/campaign_invitation_mailer_spec.rb
+++ spec/mailers/campaign_invitation_mailer_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe CampaignInvitationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "campaign_invitation_email" do
+    let(:user) { create(:user) }
+    let(:recipient) { create(:user, email: "recipient@example.com") }
+    let(:campaign) { create(:campaign, user_id: recipient.id) }
+    let(:mail) { CampaignInvitationMailer.campaign_invitation_email(user, recipient, campaign) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("You're Invited to Join #{campaign.name}")
+      expect(mail.to).to eq("#{recipient}")
+      expect(mail.from).to eq(["invitations@campaignmanager.xyz"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hi #{recipient}")
+    end
+
+    it "sends an email" do
+      expect{mail.deliver_now}.to change{ActionMailer::Base.deliveries.count}.by(1)
+    end
+  end
 end
