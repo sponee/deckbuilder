077ac015cc32559639bb0aaf9ee10a8c0700dc62
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-08-01 12:29:14 -0700

    mailer specs

--- app/models/campaign_membership.rb
+++ app/models/campaign_membership.rb
@@ -3,4 +3,5 @@ class CampaignMembership < ActiveRecord::Base
   belongs_to :campaign
 
   validates_uniqueness_of :user_id, scope: :campaign_id
+  validates_presence_of :user_id, :campaign_id
 end
--- spec/models/campaign_membership_spec.rb
+++ spec/models/campaign_membership_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CampaignMembership, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:campaign_id)}
+  it { should validate_presence_of(:user_id)}
+  it { should validate_uniqueness_of(:user_id).scoped_to(:campaign_id) }
 end
