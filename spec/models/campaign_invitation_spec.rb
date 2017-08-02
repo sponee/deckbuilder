ce5222ca5f5c746efb48a66404ff755e534cba1e
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-08-02 11:22:54 -0700

    refactor specs

--- spec/models/campaign_invitation_spec.rb
+++ spec/models/campaign_invitation_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe CampaignInvitation, type: :model do
   let!(:campaign_invitation) { build(:campaign_invitation) }
-  before { allow(campaign_invitation).to receive(:create_campaign_membership) {nil} }
-  before { allow(campaign_invitation).to receive(:send_invitation) {nil} }
+  let!(:user) { create(:user) }
+  let!(:campaign) { create(:campaign) }
 
   it { should validate_presence_of(:recipient_email) }
   it { should validate_presence_of(:sender_id) }
@@ -17,20 +17,21 @@ RSpec.describe CampaignInvitation, type: :model do
   end
 
   describe "#accept!" do
+    let!(:user) {create(:user, email: campaign_invitation.recipient_email)}
+
     it "sets its accepted attribute to true" do
       campaign_invitation.accept!
       expect(campaign_invitation.accepted).to be(true)
     end
 
-    it "triggers #create_campaign_membership" do
-      expect(campaign_invitation).to receive(:create_campaign_membership)
-      campaign_invitation.accept!
+    it "creates a new CampaignMembership" do
+      expect{campaign_invitation.accept!}.to change{CampaignMembership.count}.by(1)
     end
   end
 
-  describe "triggers #send_invitation after save" do
+  describe "after create" do
     it "sends a campaign_invitation_email" do
-      expect(campaign_invitation).to receive(:send_invitation)
+      expect_any_instance_of(CampaignInvitationMailer).to receive(:campaign_invitation_email)
       campaign_invitation.save!
     end
   end
