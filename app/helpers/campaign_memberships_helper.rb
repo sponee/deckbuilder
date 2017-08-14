af131ebb612d4782b99006aaeafbd975bb51afae
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-08-13 18:21:47 -0700

    test the new controller

--- app/helpers/campaign_memberships_helper.rb
+++ app/helpers/campaign_memberships_helper.rb
@@ -1,19 +1,11 @@
 module CampaignMembershipsHelper
   def subscription_link(campaign_membership)
-    if campaign_membership.subscribed?
-      button_to("Disable Session Reminders",
-                update_subscription_path(campaign_membership,
-                                         campaign_membership: {id: campaign_membership.id,
-                                                               receive_notifications: false}), 
-                method: :patch, 
-                class: "btn btn-default")
-    else
-      button_to("Enable Session Reminders",
-                update_subscription_path(campaign_membership,
-                                         campaign_membership: {id: campaign_membership.id,
-                                                               receive_notifications: true}),
-                method: :patch, 
-                class: "btn btn-default")
-    end
+    subscribe = campaign_membership.subscribed? ? "Disable Session Reminders" : "Enable Session Reminders" 
+    button_to(subscribe,
+              update_subscription_path(campaign_membership,
+                                       campaign_membership: {id: campaign_membership.id,
+                                                             receive_notifications: !campaign_membership.receive_notifications}), 
+              method: :patch, 
+              class: "btn btn-default")
   end
 end
