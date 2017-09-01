113670a04f3c978067faa7218f0a5a3bd22e7aa9
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-09-01 16:35:33 -0700

    use a different directory for production and testing/development

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -13,7 +13,11 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "#{model.class.to_s.underscore}s/#{model.user_id}"
+    if Rails.env.production?
+      "#{model.class.to_s.underscore}s/#{model.user_id}"
+    else
+      "test/#{model.class.to_s.underscore}s/#{model.user_id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
