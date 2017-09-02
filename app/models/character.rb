6c9683301a994d31a33c1c2d3770774880579559
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-09-01 17:57:29 -0700

    basic character specs

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,6 @@
 class Character < ActiveRecord::Base
   belongs_to :user
   mount_uploader :attachment, CharacterUploader # Tells rails to use this uploader for this model.
+
+  validates_presence_of :name, :user_id, :attachment
 end
