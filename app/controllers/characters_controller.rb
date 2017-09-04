edd3e58406c049d2b635d3329598c3d29483d7b2
Kyle Sponheim <kyle.sponheim@gmail.com>
2017-09-03 19:40:36 -0700

    rescue character creation errors

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -30,15 +30,19 @@ class CharactersController < ApplicationController
 
   def create
     @character = @user.characters.new(character_params)
-
-    respond_to do |format|
-      if @character.save!
-        format.html { redirect_to @character, notice: 'Character was successfully created.' }
-        format.json { render :show, status: :created, location: @character }
-      else
-        format.html { render :new }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
+    begin
+      respond_to do |format|
+        if @character.save!
+          format.html { redirect_to @character, notice: 'Character was successfully created.' }
+          format.json { render :show, status: :created, location: @character }
+        else
+          format.html { render :new }
+          format.json { render json: @character.errors, status: :unprocessable_entity }
+        end
       end
+    rescue => e
+      flash[:error] = e.message
+      redirect_to :back
     end
   end
 
