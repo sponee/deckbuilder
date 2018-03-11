c19803b69f0aefb50915f677b67bd19a4217c28f
sponee <kyle.sponheim@gmail.com>
2018-03-11 12:16:00 -0700

    update for rails 5

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,5 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+require_relative 'application'
 
 # Initialize the Rails application.
-CampaignManager::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'pathfinderdeckbuilder.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+Rails.application.initialize!
