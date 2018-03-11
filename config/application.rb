f09fe2d20ff47dced93e64a62db3d63f741b3990
sponee <kyle.sponheim@gmail.com>
2018-03-11 12:15:41 -0700

    update for rails 5

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,15 @@ module CampaignManager
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'pathfinderdeckbuilder.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end
