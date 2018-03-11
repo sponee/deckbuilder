60f40dec6299730f326a421836d0f4f4e62a81be
sponee <kyle.sponheim@gmail.com>
2018-03-11 12:05:58 -0700

    Revert "add rails 5.1 files"
    This reverts commit 7efd2e081bbeeac7e3d4f4bd62873b5a66a8ea6e.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
-require_relative 'application'
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Rails.application.initialize!
+CampaignManager::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'pathfinderdeckbuilder.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
