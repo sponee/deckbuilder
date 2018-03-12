e6ab678918634be20466a5239e606570bebf1fff
sponee <kyle.sponheim@gmail.com>
2018-03-11 21:17:17 -0700

    configure email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,11 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.default_url_options = { :host => 'pathfinderdeckbuilder.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
